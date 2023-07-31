401f68001937de784a1b09c9a5058b9f678f1927
Arnold Wong <aw@Arnolds-MacBook-Pro.local>
2023-07-31 16:06:32 -0500

    added GET and POST to the API

--- app.js
+++ app.js
@@ -4,4 +4,36 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Success!'));
 
+app.get("/cadets", (req,res) => {
+    const cadets = [
+    {
+      CIN: 1,
+      last_name: "Wong",
+      first_name: "Arnold",
+      preferred_language: "EN",
+    },
+    {
+        CIN: 2,
+        last_name: "Yang",
+        first_name: "Chris",
+        preferred_language: "FR",
+    },
+    {
+        CIN: 3,
+        last_name: "Willoughby",
+        first_name: "Caleb",
+        preferred_language: "EN",
+    },
+   ];
+  
+   res.json(cadets);
+});
+
+app.use(express.json()); // Use this middleware to parse JSON bodies
+
+app.post("/cadets", (req, res) => {
+    const newCadet = req.body;
+    res.json({ message: 'Cadet added successfully', cadet: newCadet });
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
