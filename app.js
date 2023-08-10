219b2a9780b5ca79d420e76ed4aaf4af0af8734c
Arnold <aw.arnoldwong@gmail.com>
2023-08-10 15:19:52 -0500

    testing routes

--- app.js
+++ app.js
@@ -9,18 +9,54 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+// express.get
 
 app.get('/', (req, res) => res.send('Success!'));
 
-app.get("/en/personal_info", (req,res) => {
-   res.json(cadets.personalInformation[req.query.cin]);
+app.get('/en/personal_info', (req, res) => {
+    res.json(cadets.personalInformation[req.query.cin]);
 });
 
+app.get('/en/cadet_info', (req, res) => {
+    res.json(cadets.cadetInformation[req.query.cin]);
+});
 
-app.use(express.json()); // Use this middleware to parse JSON bodies
+app.get('/en/membership_history', (req, res) => {
+    res.json(cadets.membershipHistory[req.query.cin]);
+});
+
+app.get('/en/appointment_history', (req, res) => {
+    res.json(cadets.appointmentHistory[req.query.cin]);
+});
+
+app.get('/en/group_history', (req, res) => {
+    res.json(cadets.groupHistory[req.query.cin]);
+});
+
+app.get('/en/awards_history', (req, res) => {
+    res.json(cadets.awardsHistory[req.query.cin]);
+});
+
+app.get('/en/qualifications', (req, res) => {
+    res.json(cadets.qualifications[req.query.cin]);
+});
+
+app.get('/en/local_training', (req, res) => {
+    res.json(cadets.localTraining[req.query.cin][req.query.program]);
+});
+
+app.get('/en/summer_training', (req, res) => {
+    res.json(cadets.summerTraining[req.query.cin]);
+});
+
+// express.use
+
+app.use(express.json()); // to parse JSON bodies
 
 
-app.post("/cadets", (req, res) => {
+// express.post
+
+app.post('/cadets', (req, res) => {
     const newCadet = req.body;
     res.json({ message: 'Cadet added successfully', cadet: newCadet });
 });
--- data.js
+++ data.js
@@ -3,16 +3,82 @@ export const personalInformation = {
         last_name: "Wong",
         first_name: "Arnold",
         preferred_language: "EN",
-      },
+    },
     1876008: {
-          last_name: "Yang",
-          first_name: "Chris",
-          preferred_language: "FR",
-      },
+        last_name: "Yang",
+        first_name: "Chris",
+        preferred_language: "FR",
+    },
     1869177: {
-          last_name: "Willoughby",
-          first_name: "Caleb",
-          preferred_language: "EN",
-      }
+        last_name: "Willoughby",
+        first_name: "Caleb",
+        preferred_language: "EN",
+    }
+}
 
+export const cadetInformation = {
+    1869177: {
+        rank: "",
+        program: "",
+    },
+}
+
+export const membershipHistory = {
+    1869177: {
+        corps_squadron: "",
+        from: "",
+        to: "",
+    },
+}
+
+export const appointmentHistory = {
+    1869177: {
+        appointment: "",
+        from: "",
+        to: "",
+    },
+}
+
+export const groupHistory = {
+    1869177: {
+        group: "",
+        from: "",
+        to: "",
+        type: "",
+    },
+}
+
+export const awardsHistory = {
+    1869177: {
+        award: "",
+        date: "",
+        type: "",
+    },
+}
+
+export const qualifications = {
+    1869177: {
+        qualification: "",
+        date: "",
+        type: "",
+    },
+}
+
+class LocalTrainingArmy {
+    constructor(element, listOfAcquiredElm) {
+        for (item in listOfAcquiredElm) {
+            this.listOfQualifications[item] = true
+        }
+    }
+    listOfQualifications = {
+        
+    }
+}
+
+
+export const localTrainingArmy = {
+    army: {
+        1869177: {
+            qualification: "",
+    }
 }
