6796facff93f0367e608596ca4cda8d507234d0d
Chris Yang <chrisyx511@gmail.com>
2023-08-15 01:55:27 -0500

    Added AuthValidate

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ import cors from 'cors'
 const app = express();
 const port = 8080;
 import * as cadets from './data.js'
+import { validateAuthToken } from './authvalidate.js';
 
+function returnAuthError(err, res) {
+    res.status(err[0])
+    res.send(err[1])
+    console.log(err[1])
+}
 const corsOptions = {
     origin: true
 }
@@ -14,39 +20,89 @@ app.use(cors(corsOptions));
 app.get('/', (req, res) => res.send('Success!'));
 
 app.get('/en/personal_info', (req, res) => {
-    res.json(cadets.personalInformation[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        if (req.query.cin === undefined | req.query.cin === "") {
+            res.json(cadets.personalInformation)
+            console.log(req.headers.authorization)
+        } else {
+            res.json(cadets.personalInformation[req.query.cin]);
+        }
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/cadet_info', (req, res) => {
-    res.json(cadets.cadetInformation[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.cadetInformation[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/membership_history', (req, res) => {
-    res.json(cadets.membershipHistory[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.membershipHistory[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/appointment_history', (req, res) => {
-    res.json(cadets.appointmentHistory[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.appointmentHistory[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/group_history', (req, res) => {
-    res.json(cadets.groupHistory[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.groupHistory[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/awards_history', (req, res) => {
-    res.json(cadets.awardsHistory[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.awardsHistory[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/qualifications', (req, res) => {
-    res.json(cadets.qualifications[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.qualifications[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/local_training', (req, res) => {
-    res.json(cadets.localTraining[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.localTraining[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 app.get('/en/summer_training', (req, res) => {
-    res.json(cadets.summerTraining[req.query.cin]);
+    validateAuthToken(req.headers.authorization).then(() => {
+        res.json(cadets.summerTraining[req.query.cin]);
+    })
+    .catch((err) => {
+        returnAuthError(err, res)
+    })
 });
 
 // express.use
---
+++ authvalidate.js
@@ -0,0 +1,48 @@
+import * as jose from 'jose'
+import axios from 'axios'
+
+const openidDiscoveryURI = 'https://login.microsoftonline.com/d2f6c0f3-9039-4a02-9e93-b489a6944a07/v2.0/.well-known/openid-configuration'
+let JWKSUri = ''
+let JWKS = ''
+const jwtOptions = {
+    audience: 'api://26581cea-19ca-4e02-8e64-68676836db6c',
+    issuer: 'https://sts.windows.net/d2f6c0f3-9039-4a02-9e93-b489a6944a07/',
+}
+async function getJWKSUri() {
+    await axios.get(openidDiscoveryURI).then((response) => {
+        JWKSUri = response.data.jwks_uri
+        JWKS = jose.createRemoteJWKSet(new URL(JWKSUri))
+        console.log('OK '+ JWKSUri)
+    })
+    .catch(err => {
+        throw err
+    })
+}
+
+
+getJWKSUri()
+
+/**
+ * Validate JWT and check authentication
+ * @param {string} bearerToken 
+ */
+export async function validateAuthToken(bearerToken) {
+    if (bearerToken === undefined) {
+        throw [401, 'Unauthenticated!']
+    }
+    let token = bearerToken.replace('Bearer ', '')
+    return await jose.jwtVerify(token, JWKS, jwtOptions).then((response) => {
+        if (response.payload.scp = 'CadetData.ReadAll') {
+            //console.log(response.payload.scp)
+            return response
+        } else {
+            throw [403, 'Invalid Scope!']
+        }
+    }).catch((err) => {
+        if (err[0] == 403) {
+            throw err
+        } else {
+            throw [401, 'Unauthenticated! ' + err]
+        }
+    })
+}
